df2d188cc2f12d9d109fec23cb41d7e85db3b445
bluephoenix5 <mia.a.lord@gmail.com>
2021-07-07 11:01:44 -0700

    Midday draft

---
+++ Technology/Computer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technology
+{
+    public class Computer
+    {
+        //Attributes
+        public string Brand;
+        public double ScreenSize;
+        
+
+        public void BrandName(string brand)
+        {
+            Brand = brand;
+            Console.WriteLine(brand);
+        }
+
+               
+        public Computer(double screenSize)
+        {
+            ScreenSize = screenSize;
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Loading...");
+        }
+    }
+}
